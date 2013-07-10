46ff50a9791d5230a7463f8eb0ceee7b1484e803
Kurt Yoder <ktygithub@yoderhome.com>
2013-07-10 09:31:55 -0400

    Add more unit tests
    Test setters, test parser results.

--- test/test_material.py
+++ test/test_material.py
@@ -8,13 +8,56 @@ class TestMaterial(unittest.TestCase):
     def setUp(self):
         pyglet.resource.path.append('@' + __name__)
         pyglet.resource.reindex()
+        self.material = pywavefront.material.Material('material')
+        self.material.set_texture('4x4.png')
 
     def testSetTexture(self):
         "Running set_texture should set a texture."
-        material = pywavefront.material.Material('material')
-        material.set_texture('4x4.png')
-        self.assertEqual(material.texture.__class__,
+        self.assertEqual(self.material.texture.__class__,
                 pywavefront.texture.Texture)
+
+    def testUnsetTexture(self):
+        "Running unset_texture should set texture to None."
+        self.material.unset_texture()
+        self.assertEqual(self.material.texture, None)
+
+    def testPadLight(self):
+        "pad_light should return known values."
+        self.assertEqual(self.material.pad_light([1.]),
+                [1., 0., 0., 0.])
+
+    def testSetAlpha(self):
+        "set_alpha should set known values."
+        self.material.set_alpha(0)
+        self.assertEqual(self.material.diffuse[3], 0.)
+        self.assertEqual(self.material.ambient[3], 0.)
+        self.assertEqual(self.material.specular[3], 0.)
+        self.assertEqual(self.material.emissive[3], 0.)
+
+    def testSetDiffuse(self):
+        "set_diffuse should set known values."
+        self.material.set_diffuse([1, 0])
+        self.assertEqual(self.material.diffuse, [1., 0., 0., 0.])
+
+    def testSetAmbient(self):
+        "set_ambient should set known values."
+        self.material.set_ambient([1, 0, 0.5, 0.2])
+        self.assertEqual(self.material.ambient, [1., 0., 0.5, 0.2])
+
+    def testSetSpecular(self):
+        "set_specular should set known values."
+        self.material.set_specular()
+        self.assertEqual(self.material.specular, [0., 0., 0., 0.])
+
+    def testSetEmissive(self):
+        "set_emissive should set known values."
+        self.material.set_emissive()
+        self.assertEqual(self.material.emissive, [0., 0., 0., 0.])
+
+class TestInvalidMaterial(unittest.TestCase):
+    def setUp(self):
+        pyglet.resource.path.append('@' + __name__)
+        pyglet.resource.reindex()
 
     def testSetInvalidTexture(self):
         "Running set_texture with a nonexistent file should raise an exception."
--- test/test_parser.py
+++ test/test_parser.py
@@ -4,7 +4,75 @@ import pyglet
 
 import pywavefront.parser
 
-class TestParser(unittest.TestCase):
+class TestParsers(unittest.TestCase):
+    def setUp(self):
+        pyglet.resource.path.append('@' + __name__)
+        pyglet.resource.reindex()
+        meshes = pywavefront.Wavefront('simple.obj')
+        self.mesh1 = meshes.mesh_list[0]
+        self.mesh2 = meshes.mesh_list[1]
+
+    def testObjName(self):
+        "Parsing an obj file with known names should set those names."
+        self.assertEqual(self.mesh1.name, 'Simple')
+        self.assertEqual(self.mesh2.name, 'SimpleB')
+
+    def testObjVertices(self):
+        "Parsing an obj file with known vertices should set those vertices."
+        # tests v, vt, vn, and f
+        material = self.mesh1.materials[0]
+        self.assertEqual(material.vertices, [
+                14.0, 15.0, 20.0, 21.0, 22.0, 0.04, 0.05, 0.06,
+                12.0, 13.0, 20.0, 21.0, 22.0, 0.01, 0.02, 0.03,
+                10.0, 11.0, 20.0, 21.0, 22.0, 0.07, 0.08, 0.09])
+        # One parser vertex comparison is quite enough, thank you!
+
+    def testObjMaterials(self):
+        "Parsing an obj file with known materials should load and assign materials."
+        # tests mtllib and usemtl
+        material1 = self.mesh1.materials[0]
+        material2 = self.mesh2.materials[0]
+        self.assertEqual(material1.name, 'Material.simple')
+        self.assertEqual(material2.name, 'Material2.simple')
+
+class TestMtlParser(unittest.TestCase):
+    def setUp(self):
+        pyglet.resource.path.append('@' + __name__)
+        pyglet.resource.reindex()
+        meshes = pywavefront.Wavefront('simple.obj')
+        self.material1 = meshes.mesh_list[0].materials[0]
+        self.material2 = meshes.mesh_list[1].materials[0]
+
+    def testMtlName(self):
+        "Parsing an obj file with known material names should set those names."
+        self.assertEqual(self.material1.name, 'Material.simple')
+        self.assertEqual(self.material2.name, 'Material2.simple')
+
+    def testMtlShininess(self):
+        "Parsing an obj file with known material shininess should set it."
+        self.assertEqual(self.material1.shininess, 1.0)
+
+    def testMtlAmbient(self):
+        "Parsing an obj file with known material ambient should set it."
+        # also tests d
+        self.assertEqual(self.material1.ambient, [0., 0., 0., 1.])
+
+    def testMtlDiffuse(self):
+        "Parsing an obj file with known material diffuse should set it."
+        # also tests d
+        self.assertEqual(self.material1.diffuse, [0.1, 0.1, 0.1, 1.])
+
+    def testMtlSpecular(self):
+        "Parsing an obj file with known material specular should set it."
+        # also tests d
+        self.assertEqual(self.material1.specular, [0.2, 0.2, 0.2, 1.])
+
+    def testMtlTextureName(self):
+        "Parsing an obj file with known material texture should set its name."
+        # also tests d
+        self.assertEqual(self.material1.texture.image_name, '4x4.png')
+
+class TestParserFailure(unittest.TestCase):
     def setUp(self):
         pyglet.resource.path.append('@' + __name__)
         pyglet.resource.reindex()
