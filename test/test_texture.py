014f7d9ef9a10264f78f42a63ffa03dd9cd4e122
Einar Forselv <eforselv@gmail.com>
2018-06-03 02:50:04 +0200

    Remove tests depending on pyglet entirely

--- test/test_texture.py
+++ test/test_texture.py
@@ -2,7 +2,6 @@ import unittest
 import os
 
 import pywavefront.texture
-import pywavefront.visualization  # power of two test
 
 
 def prepend_dir(file):
@@ -15,11 +14,6 @@ class TestTexture(unittest.TestCase):
         """For Texture objects, the image name should be the last component of the path."""
         my_texture = pywavefront.texture.Texture(prepend_dir('4x4.png'))
         self.assertEqual(my_texture.image_name, prepend_dir('4x4.png'))
-
-    # NOTE: This us using pyglet, so disabling for now
-    # def testNonPowerOfTwoImage(self):
-    #     """Texture images that have a non-power-of-two dimension should raise an exception."""
-    #     self.assertRaises(Exception, pywavefront.texture.Texture, prepend_dir('3x4.png'))
 
     def testMissingFile(self):
         """Referencing a missing texture file should raise an exception."""
