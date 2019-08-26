937608a7ecec5f1a2c68cde6ec527360d8d387d9
einarf <eforselv@gmail.com>
2019-08-26 19:36:52 +0200

    tests.utils should use pathlib

--- tests/utils.py
+++ tests/utils.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
-print()
+FIXTURE_PATH = Path(__file__).parent.absolute() / 'fixtures'
+
 
 def fixture(name: str):
     """
     Construct an absolute path to the fixture directory
     """
-    return str(Path(Path(__file__).parent.absolute(), 'fixtures', name))
+    return Path(FIXTURE_PATH, name)
