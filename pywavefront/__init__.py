549a5a993b58e4770b7064cdca2c5c4c9d76f63c
Einar Forselv <eforselv@gmail.com>
2020-01-10 12:56:38 +0100

    Bump version

--- pywavefront/__init__.py
+++ pywavefront/__init__.py
@@ -37,7 +37,7 @@ from pywavefront.exceptions import PywavefrontException
 from pywavefront.obj import ObjParser
 from pywavefront.wavefront import Wavefront
 
-__version__ = '1.3.0'
+__version__ = '1.3.1'
 
 logger = logging.getLogger("pywavefront")
 log_handler = logging.StreamHandler()
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-VERSION = '1.3.0'
+VERSION = '1.3.1'
 
 setup(
     name='PyWavefront',
